5dc1142a3c4d896d198c5f1803732376cd0da44b
Gabriel Trettel <gabrielmtrettel@gmail.com>
2019-10-08 22:47:51 -0300

    update aula 3

--- Lab2/openglwidget.h
+++ Lab2/openglwidget.h
@@ -1,17 +1,38 @@
 #ifndef OPENGLWIDGET_H
 #define OPENGLWIDGET_H
 
+#include <memory>
 #include <QWidget>
 #include <QtOpenGL>
 #include <QOpenGLWidget>
 #include <QOpenGLExtraFunctions>
 
-class OpenGLWidget : public QOpenGLWidget, protected QOpenGLExtraFunctions
+class OpenGLWidget : public QOpenGLWidget,
+        protected QOpenGLExtraFunctions
 {
     Q_OBJECT
+
+    GLuint vboVertices = 0;
+    GLuint vboColors = 0;
+    GLuint vboIndices = 0;
+
+    GLuint vao = 0;
+
+    std::unique_ptr<QVector4D []> vertices = nullptr;
+    std::unique_ptr<QVector4D []> colors = nullptr;
+    std::unique_ptr<unsigned int[]> indices= nullptr;
+
+    GLuint shaderProgram;
+
 public:
     explicit OpenGLWidget(QWidget *parent = nullptr);
     ~OpenGLWidget();
+
+    void createVBOs();
+    void createShaders();
+
+    void destroyVBOs();
+    void destroyShaders();
 
 protected:
     void initializeGL();
